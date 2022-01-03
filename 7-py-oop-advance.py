bb71410b8e97c0f3330cbfbd558a2e35f4009a87
gnl <gnl0@qq.com>
2022-01-03 11:40:14 +0800

    add metaclass

--- 7-py-oop-advance.py
+++ 7-py-oop-advance.py
@@ -395,6 +395,10 @@ print(type(h)) # <class 'Hello.Hello'>
 # 按照默认习惯，metaclass的类名总是以Metaclass结尾，以便清楚地表示这是一个metaclass
 class ListMetaClass(type):
     
+    # clazz 当前准备创建的类的对象
+    # name 类的名字
+    # bases 类继承的父类集合
+    # attrs 类的方法集合
     def __new__(clazz, name, bases, attrs):
         attrs['add'] = lambda self, val: self.append(val)
         return type.__new__(clazz, name, bases, attrs)
@@ -402,3 +406,50 @@ class ListMetaClass(type):
 # 有了ListMetaclass，我们在定义类的时候还要指示使用ListMetaclass来定制类，传入关键字参数metaclass
 class MyList(list, metaclass=ListMetaClass):
     pass
+
+L = MyList()
+L.add(1)
+print(L) # [1]
+
+
+
+
+## 动态修改有什么意义？直接在MyList定义中写上add()方法不是更简单吗？
+# ORM就是一个典型的例子。
+# 把关系数据库的一行映射为一个对象，也就是一个类对应一个表，这样，写代码更简单，不用直接操作SQL语句。
+# 要编写一个ORM框架，所有的类都只能动态定义，因为只有使用者才能根据表的结构定义出对应的类来。
+
+
+# Field类，它负责保存数据库表的字段名和字段类型：
+class Field(object):
+    def __init__(self, name, column_type):
+        self.name = name
+        self.column_type = column_type
+
+    def __str__(self):
+        return '<%s : %s>' % (self.__class__.__name__, self.name)
+
+
+# 在Field的基础上，进一步定义各种类型的Field，比如 StringField，IntegerField等等
+class StringField(Field):
+    def __init__(self, name, column_type):
+        super(StringField, self).__init__(name, 'varchar(100)')
+
+
+class IntegerField(Field):
+    def __init__(self, name, column_type):
+        super(IntegerField, self).__init__(name, 'bigint')
+
+
+# 编写 ModelMetaclass
+class ModelMetaclass(type):
+    pass
+
+# 基类Model
+class Model(object):
+    pass
+
+class User(Model):
+    pass
+
+
